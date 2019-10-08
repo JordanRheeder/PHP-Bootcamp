85f7968b8f05e6ac619bec9a712da7dca980983e
jordanrheeder <jordanrheeder@gmail.com>
2019-10-08 09:57:54 +0200

    updated

--- day01/ex02/oddeven.php
+++ day01/ex02/oddeven.php
@@ -2,18 +2,24 @@
 
 <?php
     function oddEven() {
-        while(1) {
         echo "Enter a number: ";
         $input = rtrim(fgets(STDIN));
+        if (feof(STDIN))
+            exit;
         if (!(is_numeric($input)))
         {
             echo "'$input' is not a number\n";
             oddEven();
         }
         if ($input % 2 == 0)
+        {
             echo "The number $input is even\n";
+            oddEven();
+        }
         if ($input % 2 == 1)
+        {
             echo "The number $input is odd\n";
+            oddEven();
         }
     }
     oddEven();
