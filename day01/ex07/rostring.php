4ee0bc70898ee96d4b61d97c5c89c2bb25c1dbc4
jordanrheeder <jordanrheeder@gmail.com>
2019-10-08 14:32:31 +0200

    rostring.php

---
+++ day01/ex07/rostring.php
@@ -0,0 +1,22 @@
+<?php
+    $i = 0;
+    $arr = [];
+    while ($argv[++$i])
+    {
+        $stripped = trim(preg_replace('/\s+/', ' ', $argv[$i]));
+        $split = explode(" ", $stripped);
+        for ($j = 0; $split[$j]; $j++) {
+            array_push($arr, $split[$j]);
+        }
+    }
+    $temp = array_shift($arr);
+    $len = count($arr);
+    $arr[$len - 1] = $temp;
+    for ($k = 0; $arr[$k]; $k++)
+    {
+        if ($k < $len - 1)
+            echo "$arr[$k] ";
+        else
+            echo "$arr[$k]\n";
+    }
+?>
